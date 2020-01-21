d60d4e6a57e76ac16cfb61552a4c16c1be779e39
Aditya Kudekar <akudekar@uni-muenster.de>
2020-01-21 22:37:39 +0100

    Changes
    Add GeoJSON layers

--- src/icons.js
+++ src/icons.js
@@ -5,3 +5,87 @@ export const swimming = L.divIcon({
     html: "<i class='fas fa-swimming-pool'></i>",
     iconAnchor: [11, 5],
 })
+
+export const office = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-briefcase"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const book = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-books"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const glass = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-recycle"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const hotel = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-hotel"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const cinema = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="far fa-camera-movie"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const clinic = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-clinic-medical"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const museum = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-archway"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const paper = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-paper-plane"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const school = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-graduation-cap"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const theater = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-theater-masks"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const toilet = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-restroom"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const preschool = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-baby"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const playground = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-football-ball"></i>',
+    iconAnchor: [11, 5],
+})
+
+export const sport = L.divIcon({
+    className: 'div-icon',
+    html: '<i class="fas fa-running"></i>',
+    iconAnchor: [11, 5],
+})
--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const addSwimmingFeatures = () => {
                     })
                 },
                 onEachFeature: (feature, layer) => {
-                    const popupContent = `<div class="baeder">
+                    const popupContent = `<div class="center">
                         <h2>${feature.properties.NAME}</h2>
                         <a href="${feature.properties.LINK1}" target="blank">More Information</a>
                     </div>`
@@ -42,4 +42,316 @@ const addSwimmingFeatures = () => {
         )
 }
 
+const addGovtOffices = () => {
+    fetch(config.behoerden)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.office,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addBookStations = () => {
+    fetch(config.buecherbus)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.book,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.LINK1}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+
+const addGlassContainers = () => {
+    fetch(config.glasscontainer)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.glass,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.id}</h2>
+                        // <a href="${feature.properties.LINK1}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addHotels = () => {
+    fetch(config.hotel)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.hotel,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addCinemas = () => {
+    fetch(config.cinema)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.cinema,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addClinics = () => {
+    fetch(config.clinic)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.clinic,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addMuseums = () => {
+    fetch(config.museum)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.museum,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addPapers = () => {
+    fetch(config.paper)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.paper,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addSchools = () => {
+    fetch(config.school)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.school,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.URL}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addTheaters = () => {
+    fetch(config.theater)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.theater,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addToilets = () => {
+    fetch(config.toilet)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.toilet,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        //<a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addPreschools = () => {
+    fetch(config.preschool)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.preschool,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.E_NAME}</h2>
+                        <a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addPlaygrounds = () => {
+    fetch(config.playground)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.playground,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        //<a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+const addSportHalls = () => {
+    fetch(config.sport)
+        .then(response => response.json())
+        .then(d =>
+            L.geoJSON(d, {
+                pointToLayer: function(feature, latlng) {
+                    return L.marker(latlng, {
+                        icon: icons.sport,
+                    })
+                },
+                onEachFeature: (feature, layer) => {
+                    const popupContent = `<div class="center">
+                        <h2>${feature.properties.NAME}</h2>
+                        //<a href="${feature.properties.HOMEPAGE}" target="blank">More Information</a>
+                    </div>`
+                    layer.bindPopup(popupContent)
+                },
+            }).addTo(map)
+        )
+}
+
+
 addSwimmingFeatures()
+addGovtOffices()
+addBookStations()
+addGlassContainers()
+addHotels()
+addCinemas()
+addClinics()
+addMuseums()
+addPapers()
+addSchools()
+addTheaters()
+addToilets()
+addPreschools()
+addPlaygrounds()
+addSportHalls()
+
+
