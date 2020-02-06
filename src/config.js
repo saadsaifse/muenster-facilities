412a6de498f95e55fb77397257633901b308e75f
Aditya Kudekar <akudekar@uni-muenster.de>
2020-02-06 13:18:00 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,34 +1,34 @@
 const config = {
     baeder:
-        'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Baeder&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+        'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Baeder&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
     behoerden:
-        'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Behoerden&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+        'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Behoerden&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
     buecherbus: 
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Buecherbus&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',   
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Buecherbus&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',   
  	glasscontainer:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Glascontainer&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Glascontainer&srsName=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
 	hotel:	
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Hotels&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Hotels&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	cinema:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Kino&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Kino&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	clinic:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Krankenhaeuser&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Krankenhaeuser&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	museum:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Museen&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Museen&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	paper:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Papierkorb&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Papierkorb&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	school:
- 	'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Schulen&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 	'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Schulen&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	theater:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Theater&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Theater&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	toilet:
- 	'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Toiletten&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 	'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:Toiletten&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	preschool:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:kitas01&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:kitas01&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  	playground:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:spielplaetze&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',		
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:spielplaetze&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',		
  	sport:
- 		'http://10.67.40.132:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:sportstaetten&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
+ 		'http://localhost:8080/geoserver/SII/ows?service=WFS&version=1.1.0&request=GetFeature&typeName=SII:sportstaetten&srs=EPSG:4326&format=text/javascript&outputFormat=json&format_options=callback:loadGeoJson',
  		
 }
 
